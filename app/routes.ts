a13eb3970c3625bbe714de33e8031c86faafb44a
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-13 16:55:56 -0500

    ok so NO capitals.

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes
 export default [
     index("routes/home.tsx"),
     ...prefix("start", [
-        route("/play/:roomId/", "routes/Play/play.tsx"),
+        route("/play/:roomId/", "routes/play/play.tsx"),
     ]),
 ] satisfies RouteConfig;
