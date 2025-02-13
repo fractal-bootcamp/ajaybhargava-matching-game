72ba9e3ce3d0fc3e9a1c16d009e18ad66827bc43
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-13 17:09:57 -0500

    was missing a slash in the route

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 
 ## Deployment
+
 [ ] 
--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes
 
 export default [
     index("routes/home.tsx"),
-    ...prefix("start", [
-        route("/play/:roomId/", "routes/play/play.tsx"),
+    ...prefix("/start", [
+        route(":roomId/play", "routes/play/play.tsx"),
     ]),
 ] satisfies RouteConfig;
