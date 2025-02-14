4fbcbe84b34398e3ed2f599cbccf7f1505faca25
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-14 10:56:03 -0500

    update socket

--- app/server/socket.ts
+++ app/server/socket.ts
@@ -1,5 +1,5 @@
 // In play.tsx
 import { io } from "socket.io-client";
 
-export const socket = io("https://ajays-matching-game.onrender.com");
+export const socket = io("ajaybhargava-matching-game-production.up.railway.app");
 // export const socket = io("http://localhost:3001");
