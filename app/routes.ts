1f3598cd87c852a29eeaa0c0b385b82aca3de717
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-14 10:31:45 -0500

    fix

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes
 export default [
     index("routes/home.tsx"),
     ...prefix("/start", [
-        route(":id/play", "routes/play/play.tsx"),
+        route(":id/play", "routes/play/game.tsx"),
     ]),
 ] satisfies RouteConfig;
